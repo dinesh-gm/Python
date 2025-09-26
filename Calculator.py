8c51a21d56bd0783c3580c412039bdeadcc2f876
dinesh-gm <dinesh.joey19@gmail.com>
2025-09-26 20:26:07 +0530

    Loop & Exit Option added

--- Calculator.py
+++ Calculator.py
@@ -1,26 +1,32 @@
-# Calculator.py (Version 2)
+# Calculator.py (Version 3)
 
-print("Calculator App - Version 2")
+print("Calculator App - Version 3")
 
-num1 = int(input('Enter your first number: '))
-num2 = int(input('Enter your second number: '))
+while True:
+    num1 = int(input('Enter your first number: '))
+    num2 = int(input('Enter your second number: '))
 
-print('Choose your action: \n1.Add\n2.Subtract\n3.Multiply\n4.Divide')
-calculation = input('Please enter your action: ').lower()
+    print('Choose your action: \n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit')
 
-
-if calculation=='add':
-    num3=num1+num2
-    print(f'Addition of {num1} and {num2} is {num3}')
-elif calculation=='subtract':
-    num3=num1-num2
-    print(f'Subtraction of {num1} and {num2} is {num3}')
-elif calculation=='multiply':
-    num3=num1*num2
-    print(f'Muliplication of {num1} and {num2} is {num3}')
-elif calculation=='divide':
-    if num2==0:
-        print('Cannot divide by zero')
+    calculation = input('Please enter your action: ').lower()
+    
+    if calculation=='add':
+        num3=num1+num2
+        print(f'Addition of {num1} and {num2} is {num3}')
+    elif calculation=='subtract':
+        num3=num1-num2
+        print(f'Subtraction of {num1} and {num2} is {num3}')
+    elif calculation=='multiply':
+        num3=num1*num2
+        print(f'Muliplication of {num1} and {num2} is {num3}')
+    elif calculation=='divide':
+        if num2==0:
+            print('Cannot divide by zero')
+        else:
+            num3=num1//num2
+            print(f'Division of {num1} and {num2} is {num3}')
+    elif calculation=='exit':
+        print("Exiting Calculator. Goodbye!")
+        break
     else:
-        num3=num1//num2
-        print(f'Division of {num1} and {num2} is {num3}')
+        print("Invalid choice")
