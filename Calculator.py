15e83a141ad7ab74f75a1ef86415e7efffc816c9
dinesh-gm <dinesh.joey19@gmail.com>
2025-09-26 20:08:22 +0530

    Basic calculation code added

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+# Calculator.py (Version 1)
+
+print("Calculator App - Version 1")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Result:", num1 + num2)
